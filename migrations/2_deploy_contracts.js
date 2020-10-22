ffbe480e3219f257fec911d03582c2497dda3d9b
imhihi23@gmail.com <Rno123@users.noreply.github.com>
2020-10-22 23:28:37 +0800

    Unit tests functional but syntax wrong

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
 // const SafeMath = artifacts.require("../contracts/libs/SafeMath.sol");
 // const Ownable = artifacts.require("../contracts/common/Ownable.sol");
-const DDNSService = artifacts.require("../contracts/DDNS.sol");
+const DDNSService = artifacts.require("../contracts/DDNSService.sol");
 module.exports = (deployer) => {
   deployer.deploy(DDNSService);
 };
